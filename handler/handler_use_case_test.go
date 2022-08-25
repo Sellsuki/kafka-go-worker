2569ebe189a888261b03fb4d6dc3d39ddf0d6fbe
nv4re <nv4rez@gmail.com>
2022-08-25 19:56:05 +0700

    Refactor test mock

--- handler/handler_use_case_test.go
+++ handler/handler_use_case_test.go
@@ -5,11 +5,13 @@ import (
 	"github.com/segmentio/kafka-go"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
+	"log"
 	"math/rand"
 	"testing"
 )
 
 const commitMessagesMethodName = "CommitMessages"
+const workerProcessorMethodName = "Processor"
 
 type kafkaConsumerMock struct {
 	mock.Mock
@@ -32,6 +34,24 @@ func (m *kafkaConsumerMock) Config() kafka.ReaderConfig {
 	return kafka.ReaderConfig{}
 }
 
+type workerMock struct {
+	mock.Mock
+}
+
+func (w *workerMock) Processor(ctx context.Context, msg kafka.Message) error {
+	res := w.Called(ctx, msg)
+	switch v := res[0].(type) {
+	case error:
+		return v
+	case nil:
+		return nil
+	default:
+		log.Printf("unexpected result of: %+v", v)
+		return nil
+	}
+
+}
+
 func newKafkaConsumerMock() *kafkaConsumerMock {
 	k := new(kafkaConsumerMock)
 
@@ -40,13 +60,18 @@ func newKafkaConsumerMock() *kafkaConsumerMock {
 	return k
 }
 
-func mockWorker(_ context.Context, _ kafka.Message) error {
-	return nil
+func newWorkerMock() *workerMock {
+	w := new(workerMock)
+
+	w.On(workerProcessorMethodName, mock.Anything, mock.Anything).Return(nil)
+
+	return w
 }
 
 func TestContext_UseCase_Simple(t *testing.T) {
 	m1 := newHandlerMock(1)
 	mWorker := newHandlerMock(4)
+	mockWorker := newWorkerMock()
 
 	c := Context{
 		ctx:      context.Background(),
@@ -54,7 +79,7 @@ func TestContext_UseCase_Simple(t *testing.T) {
 		handlers: []Handler{
 			m1.Handle,
 			WithAtLeastOnceCommitter,
-			WithSerialWorker(mockWorker, false, mWorker.Handle),
+			WithSerialWorker(mockWorker.Processor, false, mWorker.Handle),
 		},
 		Messages: genMessages(
 			mockMessage{1, 1, "foo"},
@@ -75,6 +100,7 @@ func TestContext_UseCase_Concurrent_Partition(t *testing.T) {
 	m2 := newHandlerMock(2)
 	mWorker := newHandlerMock(6)
 
+	mockWorker := newWorkerMock()
 	cm := newKafkaConsumerMock()
 
 	msgs := genMessages(
@@ -94,7 +120,7 @@ func TestContext_UseCase_Concurrent_Partition(t *testing.T) {
 			WithForkByPartition,
 			WithAtLeastOnceCommitter,
 			m2.Handle,
-			WithSerialWorker(mockWorker, false, mWorker.Handle),
+			WithSerialWorker(mockWorker.Processor, false, mWorker.Handle),
 		},
 		Messages: msgs,
 	}
@@ -119,6 +145,7 @@ func TestContext_UseCase_Concurrent_Key(t *testing.T) {
 	m2 := newHandlerMock(3)
 	mWorker := newHandlerMock(6)
 
+	mockWorker := newWorkerMock()
 	cm := newKafkaConsumerMock()
 
 	msgs := genMessages(
@@ -137,7 +164,7 @@ func TestContext_UseCase_Concurrent_Key(t *testing.T) {
 			WithAtLeastOnceCommitter,
 			WithForkByKey,
 			m2.Handle,
-			WithSerialWorker(mockWorker, false, mWorker.Handle),
+			WithSerialWorker(mockWorker.Processor, false, mWorker.Handle),
 		},
 		Messages: msgs,
 	}
@@ -150,4 +177,49 @@ func TestContext_UseCase_Concurrent_Key(t *testing.T) {
 
 	cm.AssertCalled(t, commitMessagesMethodName, mock.Anything, msgs)
 	cm.AssertNumberOfCalls(t, commitMessagesMethodName, 1)
+}
+
+func TestContext_UseCase_Concurrent_Key_With_Partition_Committer(t *testing.T) {
+	m1 := newHandlerMock(1)
+	m2 := newHandlerMock(3)
+	mWorker := newHandlerMock(6)
+
+	mockWorker := newWorkerMock()
+	cm := newKafkaConsumerMock()
+
+	msgs := genMessages(
+		mockMessage{1, 1, "foo"},
+		mockMessage{2, 1, "bar"},
+		mockMessage{1, 2, "foo"},
+		mockMessage{2, 2, "bar"},
+		mockMessage{2, 3, "baz"},
+		mockMessage{1, 3, "foo"},
+	)
+	c := Context{
+		ctx:      context.Background(),
+		Consumer: cm,
+		handlers: []Handler{
+			m1.Handle,
+			WithForkByPartition,
+			WithAtLeastOnceCommitter,
+			WithForkByKey,
+			m2.Handle,
+			WithSerialWorker(mockWorker.Processor, false, mWorker.Handle),
+		},
+		Messages: msgs,
+	}
+
+	assert.NoError(t, c.Start())
+
+	m1.AssertNumberOfCalls(t, handlerMockMethod, m1.expectCallCount)
+	m2.AssertNumberOfCalls(t, handlerMockMethod, m2.expectCallCount)
+	mWorker.AssertNumberOfCalls(t, handlerMockMethod, mWorker.expectCallCount)
+
+	cm.AssertCalled(t, commitMessagesMethodName, mock.Anything, []kafka.Message{
+		msgs[0], msgs[2], msgs[5],
+	})
+	cm.AssertCalled(t, commitMessagesMethodName, mock.Anything, []kafka.Message{
+		msgs[1], msgs[3], msgs[4],
+	})
+	cm.AssertNumberOfCalls(t, commitMessagesMethodName, 2)
 }
